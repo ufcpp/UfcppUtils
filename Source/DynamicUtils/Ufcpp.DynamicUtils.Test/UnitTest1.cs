c089f61454300ee39b949dd2c3fbddda77918706
niwanaga <iwanaga@orange-cube.jp>
2015-11-09 16:24:08 +0900

    Migrate to xproj/dotnet nupkg

---
+++ Source/DynamicUtils/Ufcpp.DynamicUtils.Test/UnitTest1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DynamicUtils.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestAccessorForRecursive()
+        {
+            var m = new MutableSample();
+            {
+                var d = m.AsDictionary();
+
+                d["Id"] = 1;
+                d["Time"] = DateTime.Now;
+                d["X"] = 10.0;
+                d["Y"] = 20.0;
+            }
+
+            var r1 = new RecursiveSample
+            {
+                X = new ImmutableSample(2, DateTime.Now.AddDays(1), 11, 21),
+                Y = m,
+            };
+            var r2 = new RecursiveSample
+            {
+                X = new ImmutableSample(3, DateTime.Now.AddDays(2), 12, 22),
+            };
+
+            var d1 = r1.AsDictionary();
+            var d2 = r2.AsDictionary();
+
+            d2["Y"] = d1["Y"];
+
+            Assert.NotNull(r2.Y);
+            Assert.Equal(r1.Y, r2.Y);
+
+            Assert.Equal(1, r1.Y.Id);
+            Assert.Equal(10.0, r1.Y.X);
+            Assert.Equal(20.0, r1.Y.Y);
+
+            var dx1 = d1["X"] as IDictionary<string, object>;
+            Assert.NotNull(dx1);
+            Assert.Equal(r1.X.Id, dx1["Id"]);
+            Assert.Equal(r1.X.Time, dx1["Time"]);
+            Assert.Equal(r1.X.X, dx1["X"]);
+            Assert.Equal(r1.X.Y, dx1["Y"]);
+
+            var dx2 = d2["X"] as IDictionary<string, object>;
+            Assert.NotNull(dx2);
+            Assert.Equal(r2.X.Id, dx2["Id"]);
+            Assert.Equal(r2.X.Time, dx2["Time"]);
+            Assert.Equal(r2.X.X, dx2["X"]);
+            Assert.Equal(r2.X.Y, dx2["Y"]);
+        }
+
+        [Fact]
+        public void TestAccessorForImmutable()
+        {
+            var p = new ImmutableSample(1, DateTime.Now, 10, 20);
+            var d = p.AsDictionary();
+
+            Assert.Equal(p.Id, d[nameof(p.Id)]);
+            Assert.Equal(p.Time, d[nameof(p.Time)]);
+            Assert.Equal(p.X, d[nameof(p.X)]);
+            Assert.Equal(p.Y, d[nameof(p.Y)]);
+        }
+
+        [Fact]
+        public void TestAccessorForMutable()
+        {
+            var p = new MutableSample();
+            var d = p.AsDictionary();
+
+            d[nameof(p.Id)] = 1;
+            d[nameof(p.Time)] = DateTime.Now;
+            d[nameof(p.X)] = 10.0;
+            d[nameof(p.Y)] = 20.0;
+
+            Assert.Equal(p.Id, d[nameof(p.Id)]);
+            Assert.Equal(p.Time, d[nameof(p.Time)]);
+            Assert.Equal(p.X, d[nameof(p.X)]);
+            Assert.Equal(p.Y, d[nameof(p.Y)]);
+        }
+
+        [Fact]
+        public void TestUtilForImmutable()
+        {
+            var p = new ImmutableSample(1, DateTime.Now, 10, 20);
+            var getter = ReflectionUtil.GetGetter<ImmutableSample>();
+
+            Assert.Equal(p.Id, getter(p, nameof(p.Id)));
+            Assert.Equal(p.Time, getter(p, nameof(p.Time)));
+            Assert.Equal(p.X, getter(p, nameof(p.X)));
+            Assert.Equal(p.Y, getter(p, nameof(p.Y)));
+        }
+
+        [Fact]
+        public void TestUtilForMutable()
+        {
+            var p = new MutableSample();
+            var getter = ReflectionUtil.GetGetter<MutableSample>();
+            var setter = ReflectionUtil.GetSetter<MutableSample>();
+
+            setter(p, nameof(p.Id), 1);
+            setter(p, nameof(p.Time), DateTime.Now);
+            setter(p, nameof(p.X), 10.0);
+            setter(p, nameof(p.Y), 20.0);
+
+            Assert.Equal(p.Id, getter(p, nameof(p.Id)));
+            Assert.Equal(p.Time, getter(p, nameof(p.Time)));
+            Assert.Equal(p.X, getter(p, nameof(p.X)));
+            Assert.Equal(p.Y, getter(p, nameof(p.Y)));
+        }
+    }
+}
---
+++ Source/DynamicUtils/Ufcpp.DynamicUtils/DictionaryAccessor.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+
+namespace DynamicUtils
+{
+    /// <summary>
+    /// static/extension methods for <see cref="DictionaryAccessor{T}"/>.
+    /// </summary>
+    public static class DictionaryAccessor
+    {
+        /// <summary>
+        /// create <see cref="DictionaryAccessor{T}"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public static DictionaryAccessor<T> AsDictionary<T>(this T instance) => new DictionaryAccessor<T>(instance);
+
+        /// <summary>
+        /// create <see cref="DictionaryAccessor{T}"/> by <see cref="Activator.CreateInstance(Type, object[])"/>.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public static object AsDictionary(object instance)
+        {
+            var t = instance.GetType();
+            var openType = typeof(DictionaryAccessor<>);
+            var closedType = openType.MakeGenericType(t);
+
+            return Activator.CreateInstance(closedType, instance);
+        }
+
+    }
+
+    internal interface IAccessor
+    {
+        object Instance { get; }
+    }
+
+    /// <summary>
+    /// get/set properties through IDictionary with property names as a key.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public struct DictionaryAccessor<T> : IDictionary<string, object>, IAccessor
+    {
+        private static readonly Func<T, string, object> _getter = ReflectionUtil.GetGetter<T>();
+        private static readonly Action<T, string, object> _setter = ReflectionUtil.GetSetter<T>();
+        private static readonly ICollection<string> _keys = new ReadOnlyCollection<string>(ReflectionUtil.GetPropertyNames(typeof(T)).ToArray());
+
+        private readonly T _instance;
+
+        object IAccessor.Instance => _instance;
+
+        public DictionaryAccessor(T instance) { _instance = instance; }
+
+        public object this[string key]
+        {
+            get
+            {
+                var value = _getter(_instance, key);
+
+                if (value == null
+                    || value.GetType().GetTypeInfo().IsPrimitive
+                    || value is decimal
+                    || value is string
+                    || value is DateTime
+                    || value is DateTimeOffset
+                    ) return value;
+
+                return DictionaryAccessor.AsDictionary(value);
+            }
+            set
+            {
+                var accessor = value as IAccessor;
+
+                if (accessor != null)
+                {
+                    _setter(_instance, key, accessor.Instance);
+                }
+                else
+                {
+                    _setter(_instance, key, value);
+                }
+            }
+        }
+
+        int ICollection<KeyValuePair<string, object>>.Count => _keys.Count;
+
+        bool ICollection<KeyValuePair<string, object>>.IsReadOnly => true;
+
+        ICollection<string> IDictionary<string, object>.Keys => _keys;
+
+        ICollection<object> IDictionary<string, object>.Values => this.Select(x => x.Value).ToArray();
+
+        void ICollection<KeyValuePair<string, object>>.Add(KeyValuePair<string, object> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDictionary<string, object>.Add(string key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<KeyValuePair<string, object>>.Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICollection<KeyValuePair<string, object>>.Contains(KeyValuePair<string, object> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IDictionary<string, object>.ContainsKey(string key) => _keys.Contains(key);
+
+        void ICollection<KeyValuePair<string, object>>.CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            foreach (var key in _keys)
+            {
+                yield return new KeyValuePair<string, object>(key, this[key]);
+            }
+        }
+
+        bool ICollection<KeyValuePair<string, object>>.Remove(KeyValuePair<string, object> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IDictionary<string, object>.Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IDictionary<string, object>.TryGetValue(string key, out object value) 
+        {
+            if (!_keys.Contains(key))
+            {
+                value = default(T);
+                return false;
+            }
+
+            value = this[key];
+            return true;
+        }
+    }
+}
