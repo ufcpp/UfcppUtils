1446674182fd5d7307f3585e552a1dfa3a92d4ad
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-08-09 19:34:49 +0900

    BindableWrapper のサンプル WPF 追加

---
+++ Source/BindableWrapper/SampleWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace SampleWpfApp
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new SampleData
+            {
+                PlainPoint = new Point(1, 2),
+                Point = new Point(3, 4),
+                PlainPair = new NameValuePair("abc", 123),
+                Pair = new NameValuePair("xyz", 999),
+                PlainUser = new User(1, "Alice", "4-2-8, ShibaKoen, Minato"),
+                User = new User(2, "Bob", "1-1-2, Oshiage, Sumida"),
+            };
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var data = (SampleData)DataContext;
+            data.User.SetPropertyValue("Id", 100);
+            data.PlainUser.Id = 100;
+        }
+    }
+}
---
+++ Source/BindableWrapper/SampleWpfApp/SampleData.cs
@@ -0,0 +1,66 @@
+﻿using BindableHelper;
+
+namespace SampleWpfApp
+{
+    /// <summary>
+    /// フィールドむき出しでプレーンな構造体。
+    /// </summary>
+    public struct Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    /// <summary>
+    /// プレーンな構造体。
+    /// </summary>
+    public struct NameValuePair
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public NameValuePair(string name, int value) : this()
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+
+    /// <summary>
+    /// プレーンなクラス。
+    /// </summary>
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public User(int id, string name, string address)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+        }
+    }
+
+    /// <summary>
+    /// 上記3つの型を、それぞれ生で持つのと、<see cref="BindableWrapper{T}"/>越しに持つ型。
+    /// </summary>
+    public class SampleData
+    {
+        public Point PlainPoint { get; set; }
+        public BindableWrapper<Point> Point { get; set; }
+
+        public NameValuePair PlainPair { get; set; }
+        public BindableWrapper<NameValuePair> Pair { get; set; }
+
+        public User PlainUser { get; set; }
+        public BindableWrapper<User> User { get; set; }
+    }
+}
